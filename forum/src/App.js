997d12ae234c8b49eff0b7240eff0699465706d7
“juanP1996” <parrajp1996@gmail.com>
2022-03-25 21:16:20 +0100

    feat: agregadas las rutas de los containers a app.js

--- forum/src/App.js
+++ forum/src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import './Components/Footer/Footer';
+import './Components/Header/Header';
+
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+        <Route path={'/'} element={<Home/>} />
+        <Route path={'/profile'} element={<Profile/>} />
+        <Route path={'/register'} element={<Register/>} />
+          
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
       
     </div>
   );
