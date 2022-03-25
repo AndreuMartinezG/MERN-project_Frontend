03b9b9500e1c6deced09c3f6775e97acdaccf0b3
alvmara <alvaromarzo104@gmail.com>
2022-03-25 20:30:57 +0100

    feat:creando estructura basica de components

---
+++ forum/src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Header = () => {
+
+    return (
+        <div className="designHeader">
+            soy la cabecera
+        </div>
+    )
+}
+
+export default Header;
