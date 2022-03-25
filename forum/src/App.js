23b120bf87270aff63d171611793f581a6f4acfc
“juanP1996” <parrajp1996@gmail.com>
2022-03-25 21:28:46 +0100

    feat: agregada la ruta de login a app.js

--- forum/src/App.js
+++ forum/src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import './Components/Footer/Footer';
-import './Components/Header/Header';
+import Footer from  './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
+import Home from './Containers/Home/Home';
+import Register from './Containers/Register/Register';
+import Profile from './Containers/Profile/Profile';
+import Login from './Containers/Login/Login';
 
 
 function App() {
@@ -16,7 +20,7 @@ function App() {
         <Route path={'/'} element={<Home/>} />
         <Route path={'/profile'} element={<Profile/>} />
         <Route path={'/register'} element={<Register/>} />
-          
+        <Route path={'/login'} element={<Login/>} />
         </Routes>
         <Footer/>
       </BrowserRouter>
