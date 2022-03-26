e5828693b38eb0bc026198719e891f2d675a36e3
“juanP1996” <parrajp1996@gmail.com>
2022-03-26 19:19:42 +0100

    feat:agregando control de errores al registro de password

--- src/tools.js
+++ src/tools.js
@@ -1,38 +1,62 @@
-export const checkError = (type,value) => {
+export const checkError = (type, value) => {
 
 
-    switch(type) {
+    switch (type) {
 
-        case 'email' :
+        case 'email':
 
-            if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value) ) {
-                
+            if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value)) {
+
                 return "Introduce un e-mail válido";
-            }else{
-                return "ok";
-            };
-            
-        
-        case 'nombre': 
-
-            if (! /[a-z]/gi.test(value) ) {
-                return "Introduce un nombre válido";
-            }else{
+            } else {
                 return "ok";
             };
 
-        
+
+        case 'firstName':
+
+            if (!/[a-z]/gi.test(value)) {
+                return "Introduce a valid name";
+            } else {
+                return "ok";
+            };
+
+        case 'password':
+                //^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$
+            if (/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]){6,10}$/.test(value)) {
+                return "the password must contain At least 6 characters and no more than 10, one digit and one lower case character"
+            } else {
+                return "ok";
+            };
+
+
+        case 'surname':
+
+            if (!/[a-z]/gi.test(value)) {
+                return "Introduce a valid surname";
+            } else {
+                return "ok";
+            };
+
+        case 'userName':
+
+            if (!/[a-z]/gi.test(value)) {
+                return "Introduce a valid username";
+            } else {
+                return "ok";
+            };
+
         case 'telefono':
 
-            if (! /[\d()+-]/g.test(value) ) {
+            if (!/[\d()+-]/g.test(value)) {
                 return "Introduce un telefono válido";
-            }else{
+            } else {
                 return "ok";
             };
 
         default:
             return "ok";
-        
+
 
     }
 };
