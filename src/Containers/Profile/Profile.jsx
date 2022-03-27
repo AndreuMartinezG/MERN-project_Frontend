9034b21fb9fb8c1bf8b60d0d85069c4433749bab
“AndreuMartinez” <andreu.martinez.garcia.1992@gmail.com>
2022-03-27 13:29:49 +0200

    feat: Comenzando diseño de profile

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -1,30 +1,45 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
-import {connect} from 'react-redux'
- 
+import { useNavigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import './profile.css'
- 
+
 const Profile = (props) => {
- 
+
     let navigate = useNavigate();
- 
-    useEffect(()=>{
-    console.log(props)
- 
-    },[])
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidUpdate (actualizado)
- 
-    },)
- 
- 
+
+    useEffect(() => {
+        console.log(props)
+
+    }, [])
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidUpdate (actualizado)
+
+    })
+
+
     return (
-        <div className='designProfile'>Profile</div>
-    )
+        <div className="designProfile">
+            <div className="headerProfile">
+                <img
+                    src="https://picsum.photos/id/1018/3000"
+                    className="object-cover w-full h-full rounded-b"
+                    alt="cover"
+                />
+                <div className="absolute -bottom-6">
+                    <img
+                        src="https://picsum.photos/id/1005/1000"
+                        className="object-cover border-4 border-white w-40 h-40 rounded-full"
+                        alt="cover"
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default connect((state) => ({ 
+export default connect((state) => ({
     userData: state.credentials
 }))(Profile);
