bcad86926e7f071b6b5a43827bb12b9a8ec3cd94
“juanP1996” <parrajp1996@gmail.com>
2022-03-28 17:51:02 +0200

    feat: endpoint de traer los hilos ya funciona

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,29 +1,61 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
-import {connect} from 'react-redux'
- 
+import { useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import 'antd/dist/antd.min.css';
+
 import './Home.css'
- 
+
 const Home = (props) => {
- 
+
     let navigate = useNavigate();
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidMount (montado)
- 
-    },[])
- 
-    useEffect(()=>{
-    //UseEffect equivalente a componentDidUpdate (actualizado)
- 
-    },)
- 
- 
-    return (
-        <div className='designHome'>Soy Home</div>
-    )
+
+    const [threads, setThreads] = useState([]);
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidMount (montado)
+        traerHilos();
+    }, [])
+
+    useEffect(() => {
+        //UseEffect equivalente a componentDidUpdate (actualizado)
+
+    })
+
+    const traerHilos = async () => {
+
+        try {
+            const response = await axios.get('http://localhost:5000/threads');
+            console.log(response.data);
+            setThreads(response.data);
+
+        } catch (error) {
+            console.log(error);
+        }
+
+
+    }
+
+    if (threads.length === 0) {
+        return (
+            <div className='designHome'>
+                {
+                    threads.map(hilos => {
+                        <div className='threads' key={threads._id} onClick="">
+                            <div>{threads.userName_owner}{threads.headLine}</div>
+                        </div>
+
+                    })
+                }
+            </div>
+        )
+    }else {
+        return(
+            <div className='designHome'>{"we don't have any thread at this moment"}</div>
+        )
+    }
 }
-export default connect((state) => ({ 
+
+export default connect((state) => ({
     //variables de rdx a crear
 }))(Home);
