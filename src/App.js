0cb8e9f15b486f7fd5d97a93a044ee00108215ab
“juanP1996” <parrajp1996@gmail.com>
2022-04-02 14:41:19 +0200

    feat: agregando funcionalidad al buscador de usuarios

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Register from './Containers/Register/Register';
 import Profile from './Containers/Profile/Profile';
 import Login from './Containers/Login/Login';
 import ThreadDetail from './Containers/ThreadDetail/ThreadDetail';
-import UserSearch from './Containers/UserSearch/UserSearch';
+import Users from './Containers/Users/Users';
 
 
 function App() {
@@ -26,7 +26,7 @@ function App() {
           <Route path={'/profile'} element={<Profile />} />
           <Route path={'/register'} element={<Register />} />
           <Route path={'/login'} element={<Login />} />
-          <Route path={'/userSearch'} element={<UserSearch />} />
+          <Route path={'/users'} element={<Users />} />
           <Route path={'/threadDetail'} element={<ThreadDetail />} />
         </Routes>
 
---
+++ src/Containers/Users/Users.jsx
@@ -0,0 +1,79 @@
+
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { connect } from 'react-redux';
+// import { MOVIE_DETAIL } from '../../redux/types';
+
+import './Users.css'
+
+
+function Users (props) {
+
+    let navigate = useNavigate();
+
+    
+
+    useEffect(() => {
+        console.log(props.userData);
+    }, []);
+
+
+    //useEffect custom para el hook films
+
+    useEffect(() => {
+        console.log(" CAMBIARON LOS USUARIOS, ", props.userData);
+    }, []);
+
+    const escogeUsuario = (usuarios) => {
+
+        console.log(usuarios, "he escogido este pendejo....");
+        //Guardamos la pelicula escogida en redux
+        // props.dispatch({ type: MOVIE_DETAIL, payload: pelicula });
+
+
+        //Redirigimos a movieDetail con navigate
+        navigate("/profile");
+    }
+
+    const busquedaPorusuario = async () => {
+    
+        //Axios que trae resultados....
+
+        // let config = {
+        //     headers: { Authorization: `Bearer ${props.credentials.token}` }
+        // };
+
+            try {
+                let resultados = await axios.get(`http://localhost:5000/users`);
+    
+                //Guardo en redux los resultados de las películas
+    
+                props.dispatch({type: MOVIES_TITLE, payload: resultados.data});
+    
+                setTimeout(()=>{
+                    navigate("/searchresults");
+                },500);
+    
+    
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        
+
+
+    if (props.userData._id !== undefined) {
+        return (
+            <div className="designUserSearch">
+
+                
+
+            </div>
+        )
+    }
+}
+
+export default connect((state) => ({
+    userData: state.credentials
+}))(Users);
